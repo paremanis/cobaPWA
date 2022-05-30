a09a7d8ed09aade4c9c49bdd0e3e69f55d5fda22
Afiat yulian <afiat.github@gmail.com>
2022-05-30 15:11:52 +0700

    changes service worker

--- service-worker.js
+++ service-worker.js
@@ -19,35 +19,49 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  console.log('Fetch event for ', event.request.url);
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => {
-      if (response) {
-        console.log('Found ', event.request.url, ' in cache');
-        return response;
-      }
-      console.log('Network request for ', event.request.url);
-      return fetch(event.request).then(response => {
-        // TODO 5 - Respond with custom 404 page
-        return caches.open(staticCacheName).then(cache => {
-          cache.put(event.request.url, response.clone());
-          return response;
+self.addEventListener('fetch', (event) => {
+    event.respondWith(caches.open(staticCacheName).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        const fetchedResponse = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request.url, networkResponse.clone());
+
+          return networkResponse;
         });
-      });
 
-    }).catch(error => {
-      return caches.match(event.request.url).then((cachedResponse)=>{
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+        return cachedResponse || fetchedResponse;
       });
-      // TODO 6 - Respond with custom offline page
-
-    })
-  );
+    }));
 });
+
+// self.addEventListener('fetch', event => {
+//   console.log('Fetch event for ', event.request.url);
+//   event.respondWith(
+//     caches.match(event.request)
+//     .then(response => {
+//       if (response) {
+//         console.log('Found ', event.request.url, ' in cache');
+//         return response;
+//       }
+//       console.log('Network request for ', event.request.url);
+//       return fetch(event.request).then(response => {
+//         // TODO 5 - Respond with custom 404 page
+//         return caches.open(staticCacheName).then(cache => {
+//           cache.put(event.request.url, response.clone());
+//           return response;
+//         });
+//       });
+
+//     }).catch(error => {
+//       return caches.match(event.request.url).then((cachedResponse)=>{
+//         if (cachedResponse) {
+//           return cachedResponse;
+//         }
+//       });
+//       // TODO 6 - Respond with custom offline page
+
+//     })
+//   );
+// });
 
 self.addEventListener('activate', event => {
   console.log('Activating new service worker...');
