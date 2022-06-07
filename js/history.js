7732487a5b9e1aa9d60bf2f36d76c615d153b507
Afiat yulian <afiat.github@gmail.com>
2022-06-07 16:46:34 +0700

    footer history merchant dan order page

--- js/history.js
+++ js/history.js
@@ -1,8 +1,63 @@
 function history(){
-    const data = JSON.stringify({
-        'request_type' : 'history',
+    const user_type = sessionStorage.getItem('user_type');
+    if(user_type == "user"){
+        var request_type = "history";
+    }
+    else {        
+        var request_type = "history_merchant";
+    }
+    var data = JSON.stringify({
+        'request_type' : request_type,
         'token' : sessionStorage.getItem('AuthenticationState'),
-        'id_user' : sessionStorage.getItem('id_user')
+    });
+    const url = "https://haris.globalprestasi.sch.id/api/vm.php";
+    fetch(url, {
+        method : "POST",
+        headers : {
+            'Accept' : 'application/JSON',
+        },
+        body : data,
+    })
+    .then((response) => {
+        return response.json();
+      })
+      .then((data) => {      
+        const data_history = data.data;
+          if(user_type == "user"){
+            var dataTables = data_history.map(function(value){
+                return (
+                    `<tr>
+                        <td>${value.trans_date}</td>
+                        <td>${value.merchant_name}</td>
+                    </tr>`
+                );
+            }).join('');
+          }
+          else {
+            var dataTables = data_history.map(function(value){
+                return (
+                    `<tr>
+                        <td>${value.trans_date}</td>
+                        <td>${value.jumlah}</td>
+                    </tr>`
+                );
+            }).join('');
+          }
+            
+        const tabelBody = document.querySelector("#tbody_history");
+        tabelBody.innerHTML = dataTables;
+        document.getElementById("no_data").style.display = "none";
+      })
+      .catch(function(error) { 
+        document.getElementById("table_history").style.display = "none";
+        console.log(error);
+      });  
+}
+
+function history_merchant(){
+    const data = JSON.stringify({
+        'request_type' : 'history_merchant_daily',
+        'token' : sessionStorage.getItem('AuthenticationState'),
     });
 
     const url = "https://haris.globalprestasi.sch.id/api/vm.php";
@@ -17,20 +72,24 @@ function history(){
         return response.json();
       })
       .then((data) => {
-        const data_history = data.data;
-            const dataTables = data_history.map(function(value){
-                return (
-                    `<tr>
-                        <td>${value.trans_date}</td>
-                        <td>${value.merchant_name}</td>
-                    </tr>`
-                );
-            }).join('');
-        const tabelBody = document.querySelector("#table_history");
+        const data_order = data.data;
+        const dataTables = data_order.map(function(value){
+            return (
+                `<tr>
+                    <td>${value.trans_date}</td>
+                    <td>${value.trans_code}</td>
+                    <td>${value.employee_name}</td>
+                </tr>`
+            );
+        }).join('');
+        const tabelBody = document.querySelector("#tbody_order");
         tabelBody.innerHTML = dataTables;
         document.getElementById("no_data").style.display = "none";
+        document.getElementById("date_order").innerHTML = 'Date : ' + data_order[0].trans_date;
+        console.log(data);
       })
       .catch(function(error) { 
-        document.getElementById("table_history").style.display = "none";
+        document.getElementById("table_order").style.display = "none";
+        console.log(error);
       });  
 }
