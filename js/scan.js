1acf66f9f970a4bab582e5f43c2503b513357c81
Afiat yulian <afiat.github@gmail.com>
2022-07-04 15:00:59 +0700

    scaner dengan camera belakang

--- js/scan.js
+++ js/scan.js
@@ -48,49 +48,44 @@
 const html5QrCode = new Html5Qrcode("qr-reader");
 const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     /* handle success */
-    console.log(`Code scanned = ` + decodedText, decodedResult);
-    html5QrCode.stop().then((ignore) => {
-        // QR Code scanning is stopped.
-        console.log(ignore);
-      }).catch((err) => {
-        // Stop failed, handle it.
-      });
+    // console.log(`Code scanned = ` + decodedText, decodedResult);
+    html5QrCode.stop();
     
-    // const data = JSON.stringify({
-    //     'request_type' : 'scan',
-    //     'token' : sessionStorage.getItem('AuthenticationState'),
-    //     'id_user' : sessionStorage.getItem('id_user'),
-    //     'id_merchant' : decodedText
-    // });
+    const data = JSON.stringify({
+        'request_type' : 'scan',
+        'token' : sessionStorage.getItem('AuthenticationState'),
+        'id_user' : sessionStorage.getItem('id_user'),
+        'id_merchant' : decodedText
+    });
 
-    // const url = "https://haris.globalprestasi.sch.id/api/vm.php";
-    // fetch(url, {
-    //     method : "POST",
-    //     headers : {
-    //         'Accept' : 'application/JSON',
-    //     },
-    //     body : data,
-    // })
-    // .then((response) => {
-    //     return response.json();
-    //   })
-    //   .then((data) => {
-    //       if (data.status == "error"){
-    //         sessionStorage.setItem("scan_status", "error");            
-    //         sessionStorage.setItem("error_message", data.message);
-    //         window.open('/notification.html', '_self'); 
-    //       } else {              
-    //         sessionStorage.setItem("scan_status", "success");   
-    //         sessionStorage.setItem("trans_code", data.trans_code);    
-    //         sessionStorage.setItem("merchant", data.merchant);    
-    //         sessionStorage.setItem("date_scan", data.date);             
-    //         window.open('/notification.html', '_self');   
-    //       }
-    //       console.log(data);
-    //   })
-    //   .catch(function(error) { 
-    //       console.log(error);
-    //   }); 
+    const url = "https://haris.globalprestasi.sch.id/api/vm.php";
+    fetch(url, {
+        method : "POST",
+        headers : {
+            'Accept' : 'application/JSON',
+        },
+        body : data,
+    })
+    .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+          if (data.status == "error"){
+            sessionStorage.setItem("scan_status", "error");            
+            sessionStorage.setItem("error_message", data.message);
+            window.open('/notification.html', '_self'); 
+          } else {              
+            sessionStorage.setItem("scan_status", "success");   
+            sessionStorage.setItem("trans_code", data.trans_code);    
+            sessionStorage.setItem("merchant", data.merchant);    
+            sessionStorage.setItem("date_scan", data.date);             
+            window.open('/notification.html', '_self');   
+          }
+          console.log(data);
+      })
+      .catch(function(error) { 
+          console.log(error);
+      }); 
 };
 const config = { fps: 10,  qrbox: 250 };
 
